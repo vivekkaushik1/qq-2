34e5a0944b315e79e00c0ca46f56b62b812edd51
root <root>
2021-12-17 15:06:17 +0530

    SSS

--- branchCreate.sh
+++ branchCreate.sh
@@ -3,7 +3,7 @@ echo hello world
 x=1
 y=1
 j=1
-while [ $x -le 2 ]
+while [ $x -le 1000 ]
 do
   echo "Welcome $x times"
   string=AA_PR_$((x))
@@ -15,5 +15,9 @@ do
   echo $string
   x=$(( $x + 1 ))
   sh ./createCOmmit.sh
+  rm -rf random*
+  git add .
+  git commit -m "SDDD"
+  git push
 done
 
